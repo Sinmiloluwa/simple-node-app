7d3ba66042c88e5208b06943a279d2fb66503320
Sinmiloluwa <mofeoluwae@gmail.com>
2025-04-23 11:55:58 +0100

    date validation

--- src/todo/validation/todo.schema.ts
+++ src/todo/validation/todo.schema.ts
@@ -4,6 +4,12 @@ export const createTodoSchema = z.object({
   task: z.string().min(1, "Task is required"),
   remind_at: z.string().refine((date) => !isNaN(Date.parse(date)), {
     message: "Invalid date format",
+  }).refine((val) => {
+    const now = new Date();
+    const date = new Date(val);
+    return date >= new Date(now.setHours(0, 0, 0, 0));
+  }, {
+    message: "Date cannot be less than today",
   }),
   description: z.string({required_error: "Description is required",}).min(1, "Description is required"),
 });
